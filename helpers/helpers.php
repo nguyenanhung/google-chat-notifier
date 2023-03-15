a73cbd92686bcf137493f6fddc5c0a86828d2bb0
nguyenanhung <info@nguyenanhung.com>
2023-03-15 14:39:08 +0700

    Release next-gen version v1.x

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -17,6 +17,27 @@ if (!function_exists('simple_google_chat_notifier')) {
                  ->setThreadKey($threadKey)
                  ->setMessage($message);
 
-        return $notifier->send_text_message();
+        return $notifier->sendTextMessage();
+    }
+}
+if (!function_exists('simple_google_chat_card_notifier')) {
+    function simple_google_chat_card_notifier($config, $card)
+    {
+        $spaceId = isset($config['spaceId']) ? $config['spaceId'] : null;
+        $key = isset($config['key']) ? $config['key'] : null;
+        $token = isset($config['token']) ? $config['token'] : null;
+        $threadKey = isset($config['threadKey']) ? $config['threadKey'] : null;
+        if ($spaceId === null || $key === null || $token === null) {
+            return false;
+        }
+
+        $notifier = new \nguyenanhung\Platform\Notification\GoogleChat\Notifier();
+        $notifier->setSpaceId($spaceId)
+                 ->setKey($key)
+                 ->setToken($token)
+                 ->setThreadKey($threadKey)
+                 ->setCard($card);
+
+        return $notifier->sendCardMessage();
     }
 }
--- src/Notifier.php
+++ src/Notifier.php
@@ -21,6 +21,23 @@ class Notifier
     protected $token;
     protected $message;
     protected $textResponse;
+    protected $card;
+
+    public function uuid()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), // 32 bits for "time_low"
+                       mt_rand(0, 0xffff), // 16 bits for "time_mid"
+                       mt_rand(0, 0xffff), // 16 bits for "time_hi_and_version",
+
+            // four most significant bits holds version number 4
+                       mt_rand(0, 0x0fff) | 0x4000, // 16 bits, 8 bits for "clk_seq_hi_res",
+
+            // 8 bits for "clk_seq_low",
+
+            // two most significant bits holds zero and one for variant DCE1.1
+                       mt_rand(0, 0x3fff) | 0x8000, // 48 bits for "node"
+                       mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+    }
 
     public function setConfig($config)
     {
@@ -64,12 +81,29 @@ class Notifier
         return $this;
     }
 
+    public function setCard($card)
+    {
+        $this->card = $card;
+
+        return $this;
+    }
+
     public function getTextResponse()
     {
         return $this->textResponse;
     }
 
-    public function send_text_message()
+    /**
+     * Function sendTextMessage
+     *
+     * @return bool
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 15/03/2023 35:53
+     *
+     * @see      https://developers.google.com/chat/api/guides/message-formats/text
+     */
+    public function sendTextMessage()
     {
         $webhook = self::WEBHOOKS;
         $webhook = str_replace(
@@ -85,13 +119,13 @@ class Notifier
         );
         $request = Request::execute($webhook, $params);
         if ($request === false) {
-            $this->textResponse = 'Send Message ' . $this->message . ' to is FAILED';
+            $this->textResponse = 'Send Message ' . $this->message . ' is ERROR';
 
             return false;
         }
         $response = json_decode($request, false);
         if (isset($response->error)) {
-            $this->textResponse = 'Send Message ' . $this->message . ' to is FAILED';
+            $this->textResponse = 'Send Message ' . $this->message . ' is FAILED';
 
             return false;
         }
@@ -100,5 +134,101 @@ class Notifier
         return true;
     }
 
+    /**
+     * Function sendCardMessage
+     *
+     * @return bool
+     * @author   : 713uk13m <dev@nguyenanhung.com>
+     * @copyright: 713uk13m <dev@nguyenanhung.com>
+     * @time     : 15/03/2023 35:42
+     *
+     * @see      https://developers.google.com/chat/api/guides/message-formats/cards
+     * @see      https://developers.google.com/chat/ui/read-form-data
+     * @see      https://developers.google.com/chat/ui/widgets/card-header
+     */
+    public function sendCardMessage()
+    {
+        $webhook = self::WEBHOOKS;
+        $webhook = str_replace(
+            array('{{spaceId}}', '{{key}}', '{{token}}'),
+            array($this->spaceId, $this->key, $this->token),
+            $webhook
+        );
+        if (!empty($this->threadKey)) {
+            $webhook .= '&threadKey=' . urlencode($this->threadKey);
+        }
+        $cardParams = array(
+            'cardsV2' => array(
+                'cardId' => date('YmdHis') . '-' . $this->uuid(),
+                'card'   => $this->card
+            )
+        );
+        $request = Request::execute($webhook, $cardParams);
+        if ($request === false) {
+            $this->textResponse = 'Send Message is ERROR';
 
+            return false;
+        }
+        $response = json_decode($request, false);
+        if (isset($response->error)) {
+            $this->textResponse = 'Send Message to is FAILED';
+
+            return false;
+        }
+        $this->textResponse = $response->sender->displayName . ' Send Message to Space ' . $response->space->displayName . ' is SUCCESS';
+
+        return true;
+    }
+
+    public function buildCardExample()
+    {
+        return array(
+            'header'   => array(
+                'title'        => 'Beetsoft Test',
+                'subtitle'     => 'Google Chat Test',
+                'imageUrl'     => 'https://developers.google.com/chat/images/quickstart-app-avatar.png',
+                'imageType'    => 'CIRCLE',
+                'imageAltText' => 'Avatar'
+            ),
+            'sections' => array(
+                array(
+                    'header'                    => 'Contact Info',
+                    'collapsible'               => true,
+                    'uncollapsibleWidgetsCount' => 1,
+                    'widgets'                   => array(
+                        array(
+                            'decoratedText' => array(
+                                'startIcon' => array(
+                                    'knownIcon' => "EMAIL"
+                                ),
+                                'text'      => 'contact@beetsoft.com.vn'
+                            )
+                        ),
+                        array(
+                            'decoratedText' => array(
+                                'startIcon' => array(
+                                    'knownIcon' => "EMAIL"
+                                ),
+                                'text'      => 'hungna@beetsoft.com.vn'
+                            )
+                        ),
+                        array(
+                            'buttonList' => array(
+                                'buttons' => array(
+                                    array(
+                                        'text'    => 'Share 2',
+                                        'onClick' => array(
+                                            'openLink' => array(
+                                                'url' => 'abc'
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                )
+            )
+        );
+    }
 }
